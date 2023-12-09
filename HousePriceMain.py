0779a08cf5779215aeab5fcbfdab3f8d818b54b6
goldknife <767244012@qq.com>
2023-12-09 21:07:28 +0800

    进行数据结构转化时出现错误 寻找结局方法中

--- HousePriceMain.py
+++ HousePriceMain.py
@@ -12,8 +12,8 @@ test_file = "kaggle_house_test"
 train_data = pd.read_csv("data/kaggle_house_pred_train.csv")
 test_data = pd.read_csv("data/kaggle_house_pred_test.csv")
 
-# print(test_data.shape)
-# print(test_data.shape)
+print(train_data.shape)
+print(test_data.shape)
 # 可以读取导数据 ok 12/09
 
 #print(train_data.iloc[0:4,[0,1,2,3,-3,-22,-1]])
@@ -21,19 +21,39 @@ test_data = pd.read_csv("data/kaggle_house_pred_test.csv")
 
 
 #2.训练集和测试集进行拼接
-all_features = pd.concat((train_data.iloc[:,1:-1],test_data.iloc[:,1:]))
+all_features = pd.concat((train_data.iloc[:, 1:-1], test_data.iloc[:, 1:]))
 # print(all_features.shape)
 # print(all_features.head())
     #测试数据拼接输出成功 ok 12/09
 
 # 3.处理数据，对数据进行归一化 赋予缺失均值，离散变量转独热编码
-numeric_features = all_features.dtypes[all_features.dtypes !="object"].index
+numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
 # print(numeric_features) 测试输出
-all_features[numeric_features]= all_features[numeric_features].apply(
-    lambda x: (x - x.mean()) / (x.std())
-)
-
+all_features[numeric_features] = all_features[numeric_features].apply(
+    lambda x: (x - x.mean()) / (x.std()))
 all_features[numeric_features] = all_features[numeric_features].fillna(0)
+
 all_features = pd.get_dummies(all_features,dummy_na=True)
 print(all_features.shape)
+print(all_features.head(5))
     # 完成编码 输出成功 12/09
+
+
+
+# 4，数据转化为pytorch格式 编写其他方法
+n_train = train_data.shape[0]
+
+train_features = torch.tensor(
+    all_features[:n_train].values,dtype=torch.float32
+)
+test_features=torch.tensor(
+    all_features[:n_train].values,dtype=torch.float32
+)
+train_labels=torch.tensor(
+    train_data.SalePrice.values.reshape(-1,1),dtype=torch.float32
+)
+print(train_data.SalePrice.values.reshape(-1,1).shape)
+
+# 出错 卡住 出现报错
+# TypeError: can't convert np.ndarray of type numpy.object_. The only supported types are: float64, float32, float16, complex64, complex128, int64, int32, int16, int8, uint8, and bool.
+# 返回检查 12/09
